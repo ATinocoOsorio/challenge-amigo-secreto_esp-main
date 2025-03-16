6af61f9618952ef60fd1b5eda5de8837c3e06bed
Alejandro Tinoco <alejoleox@hotmail.com>
2025-03-16 16:30:44 -0500

    Array y funcíon

---
+++ app.js
@@ -0,0 +1,7 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let Amigos = [];
+
+function agregarAmigo {
+    let input = document.getElementById('inputAmigo');
+    let amigo = input.value;
+}
